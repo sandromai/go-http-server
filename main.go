7b155957435ace7b1633515c20dbbb538dded74d
sandromai <sandromai6@gmail.com>
2023-05-20 23:31:33 -0300

    Update main

--- main.go
+++ main.go
@@ -2,19 +2,79 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"time"
 
 	"github.com/sandromai/go-http-server/models"
+	"github.com/sandromai/go-http-server/types"
+	"github.com/sandromai/go-http-server/utils"
 )
 
 func main() {
-	adminModel := &models.Admin{}
-
-	admin, err := adminModel.Authenticate("john", "123")
+	currentPath, err := os.Executable()
 
 	if err != nil {
-		fmt.Printf("(%v) %v\n", err.StatusCode, err.Message)
+		panic(err)
+	}
+
+	logger := &utils.Logger{
+		FolderPath:     filepath.Join(filepath.Dir(currentPath), "logs"),
+		FileName:       "main.log",
+		MessagesPrefix: "[MAIN]:",
+	}
+
+	adminModel := &models.Admin{}
+
+	admin, appErr := adminModel.Authenticate("john", "1234")
+
+	if appErr != nil {
+		appErr = logger.Save(
+			fmt.Sprintf("(%v) %v\n", appErr.StatusCode, appErr.Message),
+		)
+
+		if appErr != nil {
+			panic(appErr.Message)
+		}
+
 		return
 	}
 
-	fmt.Println(*admin)
+	jwt := &utils.JWT{}
+
+	adminToken, appErr := jwt.Create(&types.AdminTokenPayload{
+		AdminId:   admin.Id,
+		ExpiresAt: time.Now().Add(time.Hour * 24 * 7).Unix(),
+		CreatedAt: time.Now().Unix(),
+	})
+
+	if appErr != nil {
+		appErr = logger.Save(
+			fmt.Sprintf("(%v) %v\n", appErr.StatusCode, appErr.Message),
+		)
+
+		if appErr != nil {
+			panic(appErr.Message)
+		}
+
+		return
+	}
+
+	fmt.Println(adminToken)
+
+	authenticatedAdmin, appErr := utils.AuthenticateAdmin(adminToken)
+
+	if appErr != nil {
+		appErr = logger.Save(
+			fmt.Sprintf("(%v) %v\n", appErr.StatusCode, appErr.Message),
+		)
+
+		if appErr != nil {
+			panic(appErr.Message)
+		}
+
+		return
+	}
+
+	fmt.Println(*authenticatedAdmin)
 }
