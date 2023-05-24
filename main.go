2d4657965da86c1339826bc03e8527694ecce11c
sandromai <sandromai6@gmail.com>
2023-05-24 00:56:55 -0300

    Start http server

--- main.go
+++ main.go
@@ -2,79 +2,50 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 	"path/filepath"
-	"time"
 
-	"github.com/sandromai/go-http-server/models"
-	"github.com/sandromai/go-http-server/types"
+	"github.com/sandromai/go-http-server/routes"
 	"github.com/sandromai/go-http-server/utils"
 )
 
 func main() {
-	currentPath, err := os.Executable()
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		if request.URL.Path != "/" {
+			writer.WriteHeader(404)
 
-	if err != nil {
-		panic(err)
-	}
-
-	logger := &utils.Logger{
-		FolderPath:     filepath.Join(filepath.Dir(currentPath), "logs"),
-		FileName:       "main.log",
-		MessagesPrefix: "[MAIN]:",
-	}
-
-	adminModel := &models.Admin{}
-
-	admin, appErr := adminModel.Authenticate("john", "1234")
-
-	if appErr != nil {
-		appErr = logger.Save(
-			fmt.Sprintf("(%v) %v\n", appErr.StatusCode, appErr.Message),
-		)
-
-		if appErr != nil {
-			panic(appErr.Message)
+			return
 		}
 
-		return
-	}
+		if request.Method != "GET" {
+			writer.WriteHeader(405)
 
-	jwt := &utils.JWT{}
+			return
+		}
 
-	adminToken, appErr := jwt.Create(&types.AdminTokenPayload{
-		AdminId:   admin.Id,
-		ExpiresAt: time.Now().Add(time.Hour * 24 * 7).Unix(),
-		CreatedAt: time.Now().Unix(),
+		writer.Write([]byte("<h1>Hello world!</h1>"))
 	})
 
-	if appErr != nil {
-		appErr = logger.Save(
-			fmt.Sprintf("(%v) %v\n", appErr.StatusCode, appErr.Message),
-		)
+	http.HandleFunc("/routes/admins/login", routes.AdminLogin)
+
+	err := http.ListenAndServe(":3333", nil)
+
+	if err != nil {
+		currentPath, err := os.Executable()
+
+		if err != nil {
+			panic(err)
+		}
+
+		appErr := (&utils.Logger{
+			FolderPath:     filepath.Join(filepath.Dir(currentPath), "logs"),
+			FileName:       "main.log",
+			MessagesPrefix: "[MAIN]:",
+		}).Save(fmt.Sprintf("Server closed: %v\n", err.Error()))
 
 		if appErr != nil {
 			panic(appErr.Message)
 		}
-
-		return
 	}
-
-	fmt.Println(adminToken)
-
-	authenticatedAdmin, appErr := utils.AuthenticateAdmin(adminToken)
-
-	if appErr != nil {
-		appErr = logger.Save(
-			fmt.Sprintf("(%v) %v\n", appErr.StatusCode, appErr.Message),
-		)
-
-		if appErr != nil {
-			panic(appErr.Message)
-		}
-
-		return
-	}
-
-	fmt.Println(*authenticatedAdmin)
 }
