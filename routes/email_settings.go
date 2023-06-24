3b5c62392fb464ca6a9ddb1072b12e6b523ff014
Sandro Mai <sandromai6@gmail.com>
2023-06-23 23:35:52 -0300

    Create emailSettings routes

---
+++ routes/email_settings.go
@@ -0,0 +1,148 @@
+package routes
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"github.com/sandromai/go-http-server/middlewares"
+	"github.com/sandromai/go-http-server/models"
+	"github.com/sandromai/go-http-server/types"
+	"github.com/sandromai/go-http-server/utils"
+)
+
+func EmailSettingsList(
+	writer http.ResponseWriter,
+	request *http.Request,
+) {
+	if request.Method != "GET" {
+		utils.ReturnJSONResponse(writer, 405, nil)
+
+		return
+	}
+
+	_, appErr := middlewares.AuthenticateAdmin(request)
+
+	if appErr != nil {
+		utils.ReturnJSONResponse(
+			writer,
+			appErr.StatusCode,
+			&types.ReturnError{Error: appErr.Message},
+		)
+
+		return
+	}
+
+	emailSettings, appErr := (&models.EmailSetting{}).List()
+
+	if appErr != nil {
+		utils.ReturnJSONResponse(
+			writer,
+			appErr.StatusCode,
+			&types.ReturnError{Error: appErr.Message},
+		)
+
+		return
+	}
+
+	utils.ReturnJSONResponse(
+		writer,
+		200,
+		emailSettings,
+	)
+}
+
+func EmailSettingsUpdate(
+	writer http.ResponseWriter,
+	request *http.Request,
+) {
+	if request.Method != "PUT" {
+		utils.ReturnJSONResponse(writer, 405, nil)
+
+		return
+	}
+
+	_, appErr := middlewares.AuthenticateAdmin(request)
+
+	if appErr != nil {
+		utils.ReturnJSONResponse(
+			writer,
+			appErr.StatusCode,
+			&types.ReturnError{Error: appErr.Message},
+		)
+
+		return
+	}
+
+	var body *struct {
+		Host     string `json:"host"`
+		Port     string `json:"port"`
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	err := json.NewDecoder(request.Body).Decode(&body)
+
+	if err == io.EOF {
+		utils.ReturnJSONResponse(writer, 400, &types.ReturnError{
+			Error: "No data provided.",
+		})
+
+		return
+	}
+
+	if err != nil {
+		utils.ReturnJSONResponse(writer, 400, &types.ReturnError{
+			Error: "Invalid data.",
+		})
+
+		return
+	}
+
+	if body.Host == "" {
+		utils.ReturnJSONResponse(writer, 400, &types.ReturnError{
+			Error: "Insert the host.",
+		})
+
+		return
+	}
+
+	if body.Port == "" {
+		utils.ReturnJSONResponse(writer, 400, &types.ReturnError{
+			Error: "Insert the port.",
+		})
+
+		return
+	}
+
+	if body.Username == "" {
+		utils.ReturnJSONResponse(writer, 400, &types.ReturnError{
+			Error: "Insert the username.",
+		})
+
+		return
+	}
+
+	appErr = (&models.EmailSetting{}).Update(map[string]string{
+		"host":     body.Host,
+		"port":     body.Port,
+		"username": body.Username,
+		"password": body.Password,
+	})
+
+	if appErr != nil {
+		utils.ReturnJSONResponse(
+			writer,
+			appErr.StatusCode,
+			&types.ReturnError{Error: appErr.Message},
+		)
+
+		return
+	}
+
+	utils.ReturnJSONResponse(
+		writer,
+		200,
+		nil,
+	)
+}
